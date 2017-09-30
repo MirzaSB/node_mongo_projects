b4c253d9cb05b9757391df1a2dd39c0f5582fc35
mirzasb <mirzasbaig@nyu.edu>
2017-09-30 16:28:20 -0400

    All project and test code

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+require('./config/config');
+
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -48,6 +51,55 @@ app.get('/todos/:id', (req, res) => {
     }
   }, (e) => {
     res.status(400).send({});
+  })
+});
+
+app.delete('/todos/:id', (req, res) => {
+  // Get the id
+  const id = req.params.id;
+  //Validate the id --> Not valid? return 404.
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send({
+      error: 'The ID is not valid.'
+    });
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (todo) {
+      return res.send(todo);
+    }
+    else {
+      res.status(404).send({});
+    }
+  }, (e) => {
+    res.status(400).send({});
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send({
+      error: 'The ID is not valid.'
+    });
+  }
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  }
+  else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id,
+    {$set: body},
+    { new: true}
+  ).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   })
 });
 
