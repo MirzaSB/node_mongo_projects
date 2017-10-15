3e67224c0d91d01f29aff8e79b92eca8c4c67cb5
mirzasb <mirzasbaig@nyu.edu>
2017-10-15 19:15:29 -0400

    All new code for middleware

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -3,15 +3,15 @@ require('./config/config');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
-const {ObjectID} = require('mongodb');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -36,75 +36,87 @@ app.get('/todos', (req, res) => {
 });
 
 app.get('/todos/:id', (req, res) => {
-  const id = req.params.id;
-  if(!ObjectID.isValid(id)) {
-    return res.status(404).send({
-      error: 'The ID is not valid.'
-    });
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
   }
+
   Todo.findById(id).then((todo) => {
-    if (todo) {
-      return res.send(todo);
+    if (!todo) {
+      return res.status(404).send();
     }
-    else {
-      res.status(404).send({});
-    }
-  }, (e) => {
-    res.status(400).send({});
-  })
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.delete('/todos/:id', (req, res) => {
-  // Get the id
-  const id = req.params.id;
-  //Validate the id --> Not valid? return 404.
-  if(!ObjectID.isValid(id)) {
-    return res.status(404).send({
-      error: 'The ID is not valid.'
-    });
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
   }
+
   Todo.findByIdAndRemove(id).then((todo) => {
-    if (todo) {
-      return res.send(todo);
+    if (!todo) {
+      return res.status(404).send();
     }
-    else {
-      res.status(404).send({});
-    }
-  }, (e) => {
-    res.status(400).send({});
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
 app.patch('/todos/:id', (req, res) => {
-  const id = req.params.id;
-  const body = _.pick(req.body, ['text', 'completed']);
-  if(!ObjectID.isValid(id)) {
-    return res.status(404).send({
-      error: 'The ID is not valid.'
-    });
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
   }
+
   if (_.isBoolean(body.completed) && body.completed) {
     body.completedAt = new Date().getTime();
-  }
-  else {
+  } else {
     body.completed = false;
     body.completedAt = null;
   }
-  Todo.findByIdAndUpdate(id,
-    {$set: body},
-    { new: true}
-  ).then((todo) => {
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
+
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   })
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
 app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
